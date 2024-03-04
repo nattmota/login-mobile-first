0d5ed3b694a0f5a973a2d98a320e1fc40f375b02
nattmota <natanyujikawasaki@gmail.com>
2024-03-04 17:27:00 -0300

    att search-poke

--- js/poke-search.js
+++ js/poke-search.js
@@ -1,4 +1,22 @@
 function loadpk () {
-    const url = 'https://pokeapi.co/api/v2/pokemon';
-    
+    let url = 'https://pokeapi.co/api/v2/pokemon/blaziken';
+    fetch(url)
+        .then((response) =>  {
+            return response.json();
+        })
+        .then((data) => {
+            console.clear();
+            console.log(data);
+            document.getElementById('nome').innerHTML = data['name'];
+            document.getElementById('numero').innerHTML = data['id'];
+            let pic = data['sprites']['back_default'];
+            document.getElementById('pic').setAttribute('src', pic);
+            console.log(pic);
+        })
+
+        .catch((erro) => {
+            console.log("Erro: " + erro);
+        });
 }
+
+document.getElementById('btn1').onclick = loadpk;
